7d2fe78e674e285dab07f2d4b384d16271fb36fb
Serkan <serkaniisler@gmail.com>
2024-02-02 14:22:17 +0300

    Add project files.

---
+++ CarBook.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CarBook.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CarBookDomain/Entities/CarDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookDomain.Entities
+{
+    public class CarDescription
+    {
+        [Key]
+
+        public int DescriptionID { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public string  Details { get; set; }
+
+    }
+}
